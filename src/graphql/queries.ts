72fd6de7d30b02868762b6d881168823a0d80d16
icarovieira-hotmart <icaro.vieira@hotmart.com>
2022-12-16 19:50:05 -0300

    feat: create queries for fetch data to display

---
+++ src/graphql/queries.ts
@@ -0,0 +1,37 @@
+import { gql } from "@apollo/client"
+
+export const LOAD_CATEGORIES = gql`
+  query {
+    allCategories{
+      id,
+      name,
+      description
+    }
+  }
+`
+
+export const LOAD_CATEGORY = gql`
+  query($id: ID!) {
+    Category(id: $id){
+      name,
+      description,
+      Products {
+        id,
+        name
+      }
+    }
+  }
+`
+
+export const LOAD_PRODUCT_DETAILS = gql`
+  query($id: ID!) {
+    Product(id: $id){
+      name,
+      description,
+      color,
+      stock,
+      price,
+      category_id
+    }
+  }
+`
